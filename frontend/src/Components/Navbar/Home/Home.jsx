556ff5ed31dae8297760e6b9878d471a7cdd1067
sidra <sidrajagrawal@gmail.com>
2026-01-05 11:57:17 +0530

    New Home added

---
+++ frontend/src/Components/Navbar/Home/Home.jsx
@@ -0,0 +1,17 @@
+import bgVideo from "../../media/bg_video.mp4";
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div className="grid h-screen w-full overflow-hidden">
+            <video className="col-start-1 row-start-1 h-full w-full object-cover" src={bgVideo} autoPlay loop muted playsInline />
+        <div className="col-start-1 row-start-1 z-10 h-[12vh] text-black">
+                <Navbar />
+            </div>
+
+        </div>
+    );
+};
+
+export default Home;
+//
