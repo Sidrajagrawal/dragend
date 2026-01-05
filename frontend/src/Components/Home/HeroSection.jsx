b370f000842ce09a1f6bfdd5edfa1d4b81f2ea9e
sidra <sidrajagrawal@gmail.com>
2026-01-06 00:03:41 +0530

    Responsive

--- frontend/src/Components/Home/HeroSection.jsx
+++ frontend/src/Components/Home/HeroSection.jsx
@@ -3,14 +3,9 @@ const HeroSection = () => {
     <section className="relative h-screen w-full overflow-hidden">
 
       {/* Hero content area */}
-      <div className="min-h-[70vh] flex items-center px-6 sm:px-10 lg:px-0">
+      <div className="min-h-[70vh] flex mt-[10%] items-center md:w-[70%] px-6 sm:px-10 lg:px-0">
         <div className="lg:ml-[15%] max-w-140 z-10">
-          <h1 className="text-4xl
-+   sm:text-5xl
-+   lg:text-[64px]
-+   leading-[1.1]
-+   font-semibold
-+   text-black">
+          <h1 className="text-4xl sm:text-5xl lg:text-[64px] leading-[1.1] font-semibold text-black">
             Build Backends.
             <br />
             Drag. Drop.
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,40 @@
+import { useState } from "react";
+
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="h-full flex mt-7">
-      <div className="left w-[30%] ml-[15%] mt-[1.5%] text-3xl font-medium">
-        Dragend
-      </div>
-      <div className="right w-[40%]">
-        <ul className="text-lg flex justify-evenly font-medium cursor-pointer ">
-          <li className="mt-7 cursor-pointer
-  text-black
-  hover:bg-linear-to-r hover:from-purple-800 hover:to-pink-900
-  hover:bg-clip-text hover:text-transparent
-  transition-all duration-300">About</li>
-          <li className="mt-7 cursor-pointer
-  text-black
-  hover:bg-linear-to-r hover:from-purple-600 hover:to-pink-500
-  hover:bg-clip-text hover:text-transparent
-  transition-all duration-300">Contact</li>
+    <nav className="w-full fixed top-0 left-0 z-50">
+      <div className="mx-auto max-w-7xl px-6 py-4 flex items-center justify-between">
+        {/* Logo */}
+        <div className="text-2xl sm:text-3xl font-medium text-black">
+          Dragend
+        </div>
+
+        {/* Desktop nav */}
+        <ul className="hidden md:flex items-center gap-10 text-lg font-medium">
+          <li
+            className="
+            cursor-pointer text-black
+            hover:bg-linear-to-r hover:from-purple-800 hover:to-pink-900
+            hover:bg-clip-text hover:text-transparent
+            transition-all duration-300
+          "
+          >
+            About
+          </li>
+
+          <li
+            className="
+            cursor-pointer text-black
+            hover:bg-linear-to-r hover:from-purple-600 hover:to-pink-500
+            hover:bg-clip-text hover:text-transparent
+            transition-all duration-300
+          "
+          >
+            Contact
+          </li>
+
           <li>
             <button
               className="
