9dbed27ccebc1f03df5c7dc5be859238b9bc999e
sidra <sidrajagrawal@gmail.com>
2026-01-06 12:29:39 +0530

    Work Space

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Home/Footer';
+import Workspace from './Components/Workspace/Workspace';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/workspace" element={<Workspace />} />
       </Routes>
     </Router>
   )
