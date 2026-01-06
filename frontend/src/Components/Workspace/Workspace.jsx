ac7d5d18272a119f40ce5004ec9bba6b359f4789
sidra <sidrajagrawal@gmail.com>
2026-01-06 16:13:17 +0530

    Wrokspace Side bar

--- frontend/src/Components/Workspace/Workspace.jsx
+++ frontend/src/Components/Workspace/Workspace.jsx
@@ -1,20 +1,20 @@
 import { ReactFlow, Background, Controls, BackgroundVariant } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import { useNavigate } from 'react-router-dom';
+import LeftSidebar from './LeftSidebar';
 import CustomControls from './Custom_Controls';
 
 function Workspace() {
-  const navigate = useNavigate();
-
   return (
-    <div style={{ height: '100vh', width: '100vw', position: 'relative' }}>
-      <button onClick={() => navigate('/')} className="absolute top-4 left-4 z-50 bg-white px-4 py-2 rounded shadow cursor-pointer text-xl" style={{ pointerEvents: 'auto' }}>
-        Dragend
-      </button>
-      <div style={{ height: '100%' }}>
+    <div className="h-screen w-screen flex bg-[#1B1B1B]">
+      <LeftSidebar />
+      <div className="flex-1 bg-[#1B1B1B]">
         <ReactFlow>
-          <Background variant={BackgroundVariant.Lines} gap={80} color="grey" />
-          <CustomControls />
+          <Background
+            variant={BackgroundVariant.Dots}
+            gap={60}
+            color="white"
+          />
+          <CustomControls></CustomControls>
         </ReactFlow>
       </div>
 
