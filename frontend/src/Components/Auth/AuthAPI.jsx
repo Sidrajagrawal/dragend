05eeb13cc7e63c54a3431ded77e2d1b6d9811531
ompandey21 <omdutt2004@gmail.com>
2026-01-18 14:04:55 +0530

    Auth Integrated

---
+++ frontend/src/Components/Auth/AuthAPI.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const BASE_API = 'http://localhost:8080/api/auth';
+
+const api = axios.create({
+    baseURL: BASE_API,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export const SignUpApi = async (username, email, password, Cpassword) => {
+    try {
+        const response = await api.post('/register', { 
+            username, 
+            email, 
+            password, 
+            Cpassword 
+        });
+        return response;
+    } catch (error) {
+        throw error.response ? error.response.data : { message: "Network Error" };
+    }
+};
+
+export const VerifyOtpApi = async (email, otp) => {
+    try {
+        const response = await api.post('/verify-otp', { email, otp });
+        return response;
+    } catch (error) {
+        throw error.response ? error.response.data : { message: "Network Error" };
+    }
+};
+
+export const LoginUserApi = async (email, password) => {
+    try {
+        const response = await api.post('/login', { email, password });
+        return response.data;
+    } catch (error) {
+        throw error.response ? error.response.data : { message: "Network Error" };
+    }
+};
