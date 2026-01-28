b393837ea20b24e97b6fa768afbb03f04ee7d67a
Siddharth Raj Agrawal <142567526+Sidrajagrawal@users.noreply.github.com>
2026-01-28 13:47:59 +0530

    Sid (#5)
    * Auth Updation
    
    * Navbar Logo Deployed in Cloudinary
    
    * LeftSide Bar Logo Added
    
    * LeftSide bar updated
    
    * Cloud Updated
    
    * Cors added

--- backend/server.js
+++ backend/server.js
@@ -13,24 +13,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect Database
-if(MONGO_URL)
+if (MONGO_URL)
     connectDB(MONGO_URL);
 else
     console.log("No URL of Database Fetch");
-    
+
 
 app.use(cors({
-  origin: 'http://localhost:5173',  
-  credentials: true,                
+    origin: ['http://localhost:5173',
+        'https://dragend-h8cjcqdsfcc8gaex.centralindia-01.azurewebsites.net'
+    ],
+    credentials: true,
 }));
 app.use(cookieParser());
 
 //Base API
-app.use('/api',indexRoute)
+app.use('/api', indexRoute)
 
 //Testing Route
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("<h1>Welcome to Backendless</h1>")
 })
 
-app.listen(PORT,()=>{console.log(`Server at: http://localhost:${PORT}/`)});
+app.listen(PORT, () => { console.log(`Server at: http://localhost:${PORT}/`) });
